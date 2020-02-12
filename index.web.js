f49a951c6387459ddbccc89ca0c12115a4d397cc
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2020-02-12 15:33:49 +0100

    Keychain web fix Version bump

--- index.web.js
+++ index.web.js
@@ -5,34 +5,36 @@ function getOptionsArgument(serviceOrOptions) {
 }
 
 export function setGenericPassword(username, password, serviceOrOptions) {
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     try {
-      localStorage.setItem(getOptionsArgument(serviceOrOptions), password);
+      const service = getOptionsArgument(serviceOrOptions);
+      let value = { service, username, password };
+      localStorage.setItem(service, JSON.stringify(value));
       resolve();
     } catch (error) {
       reject(error);
     }
-  }); 
+  });
 }
 
 export function resetGenericPassword(serviceOrOptions) {
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     try {
       localStorage.removeItem(getOptionsArgument(serviceOrOptions));
       resolve();
     } catch (error) {
       reject(error);
     }
-  }); 
+  });
 }
 
 export function getGenericPassword(serviceOrOptions) {
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     try {
       const data = localStorage.getItem(getOptionsArgument(serviceOrOptions));
-      resolve(data ? { password: data } : null);
+      resolve(JSON.parse(data));
     } catch (error) {
       reject(error);
     }
-  }); 
+  });
 }
